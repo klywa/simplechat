1d3f7a8efd76fa325af326f29410000b1a5a703d
Klywa <klywa.m@gmail.com>
2025-02-08 17:22:59 +0800

    新增修改英雄名

--- scripts/ui/character_button.gd
+++ scripts/ui/character_button.gd
@@ -9,11 +9,18 @@ var chat : Chat
 @onready var name_label = $VBoxContainer/Name
 @onready var lane_label = $VBoxContainer/Lane
 @onready var name_button = $VBoxContainer/Name/NameButton
+@onready var hero_button = $VBoxContainer/Lane/LaneButton
 
 @onready var character_info_panel := $CharacterInfoPanel
 @onready var setting_info := $CharacterInfoPanel/PanelContainer/MarginContainer/ScrollContainer/VBoxContainer/Setting
 @onready var style_info := $CharacterInfoPanel/PanelContainer/MarginContainer/ScrollContainer/VBoxContainer/Style
 @onready var example_info := $CharacterInfoPanel/PanelContainer/MarginContainer/ScrollContainer/VBoxContainer/Example
+
+@onready var hero_panel := $HeroPanel
+@onready var new_hero_label := $HeroPanel/PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/Hero
+@onready var new_lane_label := $HeroPanel/PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/Lane
+@onready var confirm_hero_change_button := $HeroPanel/PanelContainer/MarginContainer/VBoxContainer/HBoxContainer2/Confirm
+@onready var cancel_hero_change_button := $HeroPanel/PanelContainer/MarginContainer/VBoxContainer/HBoxContainer2/Cancel
 
 
 signal character_left_clicked(character: NPC)
@@ -24,6 +31,10 @@ const CHARACTER_INFO_PANEL_SCENE = preload("res://scenes/ui/character_info_panel
 func _ready() -> void:
 	button.pressed.connect(on_pressed)
 	name_button.pressed.connect(on_name_button_pressed)
+
+	hero_button.pressed.connect(on_hero_button_pressed)
+	confirm_hero_change_button.pressed.connect(on_confirm_hero_change_button_pressed)
+	cancel_hero_change_button.pressed.connect(on_cancel_hero_change_button_pressed)
 
 func init(character_in: NPC, chat_in: Chat) -> void:
 	character = character_in
@@ -45,3 +56,17 @@ func on_name_button_pressed() -> void:
 	style_info.text = character.npc_style
 	example_info.text = character.npc_example
 	character_info_panel.visible = true
+
+func on_hero_button_pressed() -> void:
+	hero_panel.visible = true
+	new_hero_label.text = character.hero_name
+	new_lane_label.text = character.hero_lane
+
+func on_confirm_hero_change_button_pressed() -> void:
+	character.hero_name = new_hero_label.text
+	character.hero_lane = new_lane_label.text
+	hero_panel.visible = false
+	init(character, chat)
+
+func on_cancel_hero_change_button_pressed() -> void:
+	hero_panel.visible = false
