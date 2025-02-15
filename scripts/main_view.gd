c45d125ed3c7e16bac94ba2a801462c18ee39ed0
Klywa <klywa.m@gmail.com>
2025-02-15 18:04:23 +0800

    支持双模式

--- scripts/main_view.gd
+++ scripts/main_view.gd
@@ -3,6 +3,7 @@ extends Control
 @export_enum("single", "pipeline") var mode : String = "single"
 @export var config_path : String = "res://config/default.json"
 @export var ai_server_url : String = "http://9.208.245.48:8000/model_chat"
+@export var ai_pipeline_url : String = "http://30.50.188.179:8080/api/smartnpc"
 
 @onready var player_icon := $VBoxContainer/ChatPanel/Sidebar/MarginContainer/VBoxContainer/PlayerIcon
 @onready var chat_view : ChatView = $VBoxContainer/ChatPanel/ChatWindow/ChatView
@@ -12,4 +13,7 @@ extends Control
 func _ready() -> void:
 	if config_path.length() > 0:
 		GameManager.init(self, config_path)
-	AIManager.init(ai_server_url)
+	if mode == "pipeline":
+		AIManager.init(ai_pipeline_url)
+	else:
+		AIManager.init(ai_server_url)
